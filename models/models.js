620e860e173180dd6a39e7825f57ad9fe65e49f9
jjohanzee <inet.swor@gmail.com>
2015-07-18 19:49:39 +0200

    Preparación postgres. Carga de variables de entorno para uso con node

--- models/models.js
+++ models/models.js
@@ -2,13 +2,30 @@
 
 var path = require('path');
 
+// Postgres DATABASE_URL = postgres://user:passwd@host:port/database
+// SQLite   DATABASE_URL = sqlite://:@:/
+var url = process.env.DATABASE_URL.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
+var DB_name     = (url[6] || null);
+var user        = (url[2] || null);
+var pwd         = (url[3] || null);
+var protocol    = (url[1] || null);
+var dialect     = (url[1] || null);
+var port        = (url[5] || null);
+var host        = (url[4] || null);
+var storage     = process.env.DATABASE_STORAGE;
+
 //Cargar ORM
 var Sequelize = require('sequelize');
 
 // Definir tipo y nombre de BDD
-var sequelize = new Sequelize(null, null, null,
-            {dialect: "sqlite", storage: "quiz.sqlite"}
-        );
+var sequelize = new Sequelize(DB_name, user, pwd, {
+                    dialect:    dialect,
+                    protocol:   protocol,
+                    port:       port,
+                    host:       host,
+                    storage:    storage,    // solo SQLite (.env)
+                    omitNull:   true        // solo Postgres
+});
 
 //Importamos definición de la tabla Quiz
 var Quiz = sequelize.import(path.join(__dirname, 'quiz'));
@@ -18,8 +35,6 @@ exports.Quiz = Quiz;
 
 // Creación BDD y opcionalmente inicialización de la tabla Quiz
 // sync sincroniza las definiciones del modelo con la BDD
-// (utilizamos la forma obsoleta de manejo de callbacks en sequelize con
-// success. En la actualidad se utilizan promises)
 sequelize.sync().then(function(){
    //comprobamos si la tabla está vacía y si lo está añadimos las dos
    //primeras preguntas
