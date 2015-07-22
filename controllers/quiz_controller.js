074c4c12b2474b07ddb720282aea64850a9d8802
jjohanzee <inet.swor@gmail.com>
2015-07-22 20:12:29 +0200

    Lista de preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,27 +1,29 @@
 // Importamos el modelo
 var models = require('../models/models.js');
 
-//GET /quizes/question
-exports.question = function(req,res) {
-    //recuperamos preguntas y mostramos la primera
-    models.Quiz.findAll().then(function(quiz){
-       res.render('quizes/question', {pregunta: quiz[0].pregunta});
-    });
-};
-
-//GET /quizes/answer
+//GET /quizes/:id/answer
 exports.answer = function(req, res) {
-    
-    //recuperamos respuestas y comparamos con la primera
-    models.Quiz.findAll().then(function(quiz){
-       var r = '';
-       if (req.query.respuesta === quiz[0].respuesta) {
+    models.Quiz.findById(req.params.quizId).then(function(quiz) {
+        var r = '';
+        if (req.query.respuesta === quiz.respuesta) {
            r = 'Correcto';
-       } else {
+        } else {
            r = 'Incorrecto';
-       }
-       res.render('quizes/answer', {respuesta: r});
+        }
+        res.render('quizes/answer', {quiz: quiz, respuesta: r});    
     });
 };
 
+//GET /quizes/:id
+exports.show = function(req,res) {
+    models.Quiz.findById(req.params.quizId).then(function(quiz) {
+                res.render('quizes/show', {quiz: quiz});
+            });
+};
 
+//GET /quizes
+exports.index = function(req, res) {
+    models.Quiz.findAll().then(function(quizes){
+        res.render('quizes/index.ejs', {quizes: quizes});
+    });
+};
